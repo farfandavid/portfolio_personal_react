13e0202d13858b1bac238cad543a31cda36edcfb
farfandavid <70915094+farfandavid@users.noreply.github.com>
2023-02-28 01:13:40 -0300

    fix exist links

--- src/components/projects/components/ProjectCard.jsx
+++ src/components/projects/components/ProjectCard.jsx
@@ -5,14 +5,17 @@ function ProjectCard({ name, linkGitHub, linkDeploy, image }) {
         <img src={image} alt="Project 1" />
         <div className="overlay">
           <p>{name}</p>
-          <div className="icons-container">
-            <a href={linkGitHub} target="_blank" rel="noopener noreferrer">
-              <i className="bi bi-github"></i>
-            </a>
-            <a href={linkDeploy} target="_blank" rel="noopener noreferrer">
-              <i className="bi bi-laptop"></i>
-            </a>
-          </div>
+          {linkGitHub !== '' ?
+            <div className="icons-container">
+              <a href={linkGitHub} target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-github"></i>
+              </a>
+              <a href={linkDeploy} target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-laptop"></i>
+              </a>
+            </div>
+            : null}
+
         </div>
       </div>
     </div>
